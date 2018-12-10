ff4d35a95bdeb70d41dc0118598c01e810bbcf4c
hmj <1004033345@qq.com>
2018-12-10 17:24:20 +0800

    登录mock

--- src/mock/data/user.js
+++ src/mock/data/user.js
@@ -5,3 +5,21 @@
  * @Version:1.0
  */
 import mock from "mockjs"
+
+const loginUsers = [
+  //登录用户
+  {
+    id:1,
+    account:"admin",
+    password:"admin",
+    name:"admin"
+  },
+  {
+    id:2,
+    account:"jay",
+    password:"123456",
+    name:"jay"
+  }
+];
+
+export {loginUsers}
--- src/mock/mock.js
+++ src/mock/mock.js
@@ -5,4 +5,49 @@
  * @Version:1.0
  */
 import axios from "axios";
-import MockAdapter from "axios-mock-adapter"
+import MockAdapter from "axios-mock-adapter";
+import {loginUsers} from "./data/user";
+
+export default {
+  bootstrap(){
+    //定义一个mock adapter的实例
+    let mock = new MockAdapter(axios)
+    mock.onGet('/success').reply(200,{
+      msg:"success"
+    });
+    mock.onGet('/error').reply(500,{
+      msg:"error"
+    });
+    mock.onPost("/login").reply(config=>{
+      // alert(JSON.stringify(config))
+      let {account,password} = JSON.parse(config.data)
+      return new Promise((resolve,reject)=>{
+        let user = null;
+        setTimeout(()=>{
+          let hasUser = loginUsers.some(u=>{
+            if(u.account == account && u.password == password){
+              // alert(JSON.stringify(u))
+              user = u;
+              user.password = undefined
+              return true
+            }else {
+              return false
+            }
+          });
+          if(hasUser){
+            resolve([200,{
+              code:200,
+              msg:"登录成功",
+              user:user
+            }])
+          }else {
+            resolve([200,{
+              code:500,
+              msg:"账号或密码错误"
+            }])
+          }
+        },500)
+      })
+    });
+  }
+}
